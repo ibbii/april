0a307023c6e61e98eab2e6ed3c02cf9559a0832c
AbiLop <abilopga@gmail.com>
2023-10-19 15:51:21 -0600

    step 2 created function

--- temp.py
+++ temp.py
@@ -1,3 +1,14 @@
-# Program on how to change clecius to Farenheit
-fah = 25 * 9/5 + 32
-print(f"25 defrees is {fah} Farenheit")
+"""
+Program to Convert Celcius To Fahrenheit
+"""
+
+def celcius_to_fahrenheit(celcius):
+    """ convert value given to fahrenheit """
+    fah = celcius * 9/5 + 32
+    return fah
+
+
+celcius = 25
+fah = celcius_to_fahrenheit(celcius)
+print(f"{celcius} degrees is {fah} Farenheit")
+
